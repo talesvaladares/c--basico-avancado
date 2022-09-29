4cf356577a82422514b6e71b4bb23cb55bf498db
Tales Eduardo <tales.e.valadares@gmail.com>
2022-09-29 09:43:33 -0300

    ok

---
+++ ConsoleApp1/fundamentos/Comentarios.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoCSharp.fundamentos {
+    /// <summary>
+    /// 
+    /// </summary>
+    internal class Comentarios {
+        public static void Executar() {
+            
+            Console.WriteLine("Código claro é sempre melhor!");
+
+            //control k U => comenta
+            //control k c => descomenta
+        }
+    }
+}
